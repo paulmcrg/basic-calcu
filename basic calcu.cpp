fa51dfe73f6c253283d743e79f75eb8a9ac6308c
jpmcrg <paul@mcrg.com>
2020-10-11 18:52:55 +0800

    first commit

---
+++ basic calcu.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	
+	
+	char cal;
+	int num1,num2,total;
+	
+	cout<<"choose a operation to perform + - * /"<<endl;
+	cin>>cal;
+	
+	cout<<"enter first number"<<endl;
+	cin>>num1;
+	
+	cout<<"enter second number "<<endl;
+	cin>>num2;
+
+		
+	switch(cal){
+		
+		case '+': total = num1 + num2;
+		break;
+		
+		case '-': total = num1 - num2;
+		break;
+		
+		case '*': total = num1 * num2;
+		break;
+		
+		case '/':total = num1 / num2;
+		break;
+		
+		default:
+			cout<<"error operation"<<endl;
+			
+	}
+		cout<<"total :"<<total;
+}
